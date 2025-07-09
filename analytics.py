88281a4bfe43b3798203ec400c8eb479f80acfd0
KangoChipaila <kangomsindechipailam@gmail.com>
2025-07-09 23:06:49 +0200

    added logic for pie charts in sales

--- analytics.py
+++ analytics.py
@@ -1,8 +1,9 @@
 import plotly.graph_objects as go
 import pandas as pd
 
+extracted_data = pd.read_csv("./spreadsheet_datasets/sales_data_sample.csv", encoding="cp1252")
+
 def generate_sales_trend():
-    extracted_data = pd.read_csv("./spreadsheet_datasets/sales_data_sample.csv", encoding="cp1252")
 
     extracted_data["ORDERDATE"] = pd.to_datetime(extracted_data["ORDERDATE"], format = "%m/%d/%Y %H:%M", errors = "coerce")
 
@@ -20,4 +21,28 @@ def generate_sales_trend():
                     xaxis_title = 'Month-Year',
                     yaxis_title = 'Sales (1 = 1000 Kwacha)')
     
+    return fig.to_plotly_json()
+
+def generate_goods_performance_pchart():
+
+    goods_performance = extracted_data.groupby("PRODUCTLINE")["QUANTITYORDERED"].sum().reset_index()
+
+    fig = go.Figure(data = [go.Pie(labels = (goods_performance["PRODUCTLINE"].astype(str)).to_list(), 
+                                   values = goods_performance["QUANTITYORDERED"].to_list())])
+    
+    fig.update_layout(title = "Goods Performance")
+
+    return fig.to_plotly_json()
+
+def generate_customer_expenditure_distribution_pchart():
+
+    customer_expediture = extracted_data.groupby("CUSTOMERNAME")["SALES"].sum().reset_index()
+
+    top_10_customers = customer_expediture.head(10)
+
+    fig = go.Figure(data = [go.Pie(labels = (top_10_customers["CUSTOMERNAME"].astype(str)).to_list(), 
+                                   values = top_10_customers["SALES"].to_list())])
+
+    fig.update_layout(title = "Customer By Expenditure")
+
     return fig.to_plotly_json()
--- app.py
+++ app.py
@@ -9,8 +9,15 @@ def index():
 
 @app.route('/sales-overview')
 def sales_overview():
+
     sales_trend_graph = analytics.generate_sales_trend()
-    return render_template('sales_overview.html', sales_trend_graph = sales_trend_graph)
+    goods_performance_pie_chart = analytics.generate_goods_performance_pchart()
+    customer_expenditure_pie_chart = analytics.generate_customer_expenditure_distribution_pchart()
+
+    return render_template('sales_overview.html', 
+                           sales_trend_graph = sales_trend_graph, 
+                           goods_performance_pie_chart = goods_performance_pie_chart,
+                           customer_expenditure_pie_chart = customer_expenditure_pie_chart)
 
 if __name__ == '__main__':
     app.run(debug=True)
