678fbcf4e153db67392fae2a686e86e5b8724a3b
KangoChipaila <kangomsindechipailam@gmail.com>
2025-09-03 15:46:46 +0200

    Added barcode detection logic

--- asset_upload_module.py
+++ asset_upload_module.py
@@ -17,6 +17,26 @@ def barcode_scanner():
             print("Error: Failed to read camera frame")
             break
 
+        grayscale_feed = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        barcodes = decode(grayscale_feed)
+
+        for barcode in barcodes:
+
+            (x, y, w, h) = barcode.rect
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+            barcode_data = barcode.data.decode("utf-8")
+            barcode_type = barcode.type
+
+            info = f"Barcode Type: {barcode_type}, Data: {barcode_data}"
+            cv2.putText(frame, info, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+
+            if (barcode_data != "" and barcode_type != ""):
+
+                print(info)    
+                exit()
+
         cv2.imshow("Live Camera Feed (NOTE: Press 'q' to Quit)", frame)
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
